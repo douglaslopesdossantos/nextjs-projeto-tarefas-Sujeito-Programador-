5b411fa675ab9945b4535065b71d0ab15d2be76a
Douglas Lopes dos Santos <contato.geardev@gmail.com>
2024-03-06 23:34:31 -0300

    chore: header component created

---
+++ src/components/header/index.tsx
@@ -0,0 +1,23 @@
+import styles from './styles.module.css'
+import Link from 'next/link'
+
+export function Header() {
+    return (
+        <header className={styles.header} >
+            <section className={styles.content}>
+                <nav className={styles.nav}>
+                    <Link href="/">
+                        <h1 className={styles.logo}>
+                            Tarefas
+                            <span>+</span>
+                        </h1>
+                    </Link>
+                    <Link className={styles.link} href="/dashboard">Meu Painel</Link>
+                </nav>
+                <button className={styles.logginButton}>
+                    Acessar
+                </button>
+            </section>
+        </header>
+    )
+}
