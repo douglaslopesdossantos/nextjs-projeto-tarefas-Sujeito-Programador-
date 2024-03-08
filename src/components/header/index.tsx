b6b888fcc0ba8bb7d29f5961811ee2cad0b4ea36
Douglas Lopes dos Santos <contato.geardev@gmail.com>
2024-03-08 12:00:55 -0300

    chore: auth google done and dashboard screen created

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,12 @@
 import styles from './styles.module.css'
+import { useSession, signIn, signOut } from 'next-auth/react'
 import Link from 'next/link'
 
 export function Header() {
+
+    const { data: session, status } = useSession()
+    console.log(session, status)
+
     return (
         <header className={styles.header} >
             <section className={styles.content}>
@@ -12,11 +17,18 @@ export function Header() {
                             <span>+</span>
                         </h1>
                     </Link>
-                    <Link className={styles.link} href="/dashboard">Meu Painel</Link>
+                    {session?.user && <Link className={styles.link} href="/dashboard">Meu Painel</Link>}
                 </nav>
-                <button className={styles.logginButton}>
-                    Acessar
-                </button>
+                {status === "loading" ? (<></>) :
+                    session ? (
+                        <button className={styles.logginButton} onClick={() => signOut()} >
+                            Olá, {session?.user?.name}
+                        </button>
+                    ) : (
+                        <button className={styles.logginButton} onClick={() => signIn()} >
+                            Acessar
+                        </button>
+                    )}
             </section>
         </header>
     )
